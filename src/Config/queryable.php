a1163d43dd3176eef427998577c16840bf59dc6d
Stephen Lake <stephen@closurecode.com>
2018-09-17 04:39:52 +0200

    Update config

--- src/Config/queryable.php
+++ src/Config/queryable.php
@@ -7,12 +7,22 @@ return [
     | Query Key Names
     |--------------------------------------------------------------------------
     |
+    | You can change the name of the query parameter keys here if you have a
+    | reserved word for other queries or prefer something else.
     |
+    | These names are used in triggering whether or not to run search or filters
+    | on an HTTP call. For example:
+    \
+    | http://localhost?search=someSearchTerm will trigger the search as long as
+    | the 'search' query parameter is present.
+    |
+    | http://localhost?filters=firstname=test will trigger the filters as long as
+    | the 'filters' query parameter is present.
     |
     */
 
     'searchKeyName' => 'search',
-    'filterKeyName' => 'filters',
+    'filterKeyName' => 'filter',
 
     /*
     |--------------------------------------------------------------------------
